64c567a8519be9a45ddd6400ba2e169563c24891
wende363apps-collab <wende363apps@gmail.com>
2025-09-03 00:34:12 +0300

    Update TripRepository.kt

--- app/src/main/java/com/truckmanager/app/TripRepository.kt
+++ app/src/main/java/com/truckmanager/app/TripRepository.kt
@@ -1,16 +1,7 @@
 package com.truckmanager.app
 
-import kotlinx.coroutines.flow.Flow
-
 class TripRepository(private val tripDao: TripDao) {
-
-    val allTrips: Flow<List<Trip>> = tripDao.getAllTrips()
-
-    suspend fun insert(trip: Trip) {
-        tripDao.insertTrip(trip)
-    }
-
-    suspend fun delete(trip: Trip) {
-        tripDao.deleteTrip(trip)
-    }
+    suspend fun insertTrip(trip: Trip) = tripDao.insertTrip(trip)
+    suspend fun getAllTrips(): List<Trip> = tripDao.getAllTrips()
+    suspend fun deleteTrip(trip: Trip) = tripDao.deleteTrip(trip)
 }
