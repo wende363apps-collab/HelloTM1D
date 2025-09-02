73fd7b045a6a049a434d67a6e0ce3dd0a0cb277b
wende363apps-collab <wende363apps@gmail.com>
2025-09-02 21:46:16 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/truckmanager/app/AppDatabase.kt
+++ app/src/main/java/com/truckmanager/app/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.truckmanager.app
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Trip::class], version = 1)
+@Database(entities = [Trip::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun tripDao(): TripDao
 }
