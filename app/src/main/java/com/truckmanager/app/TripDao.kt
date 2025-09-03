ae997e9851ee39fcd112c82aaee754582e87b3b6
wende363apps-collab <wende363apps@gmail.com>
2025-09-03 12:49:40 +0300

    Update TripDao.kt

--- app/src/main/java/com/truckmanager/app/TripDao.kt
+++ app/src/main/java/com/truckmanager/app/TripDao.kt
@@ -1,16 +1,16 @@
 package com.truckmanager.app
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TripDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTrip(trip: Trip)
 
-    @Query("SELECT * FROM trips ORDER BY id DESC")
-    fun getAllTrips(): Flow<List<Trip>>
-
     @Delete
     suspend fun deleteTrip(trip: Trip)
+
+    @Query("SELECT * FROM trips ORDER BY id DESC")
+    fun getAllTrips(): LiveData<List<Trip>>
 }
