d81939a16fc12c82b1aea7ec859db883dcda9f14
wende363apps-collab <wende363apps@gmail.com>
2025-09-22 20:02:29 +0300

    Update TripRepository.kt

--- app/src/main/java/com/truckmanager/app/TripRepository.kt
+++ app/src/main/java/com/truckmanager/app/TripRepository.kt
@@ -2,16 +2,9 @@ package com.truckmanager.app
 
 import androidx.lifecycle.LiveData
 
-class TripRepository(private val dao: TripDao) {
+class TripRepository(private val tripDao: TripDao) {
+    val allTrips: LiveData<List<Trip>> = tripDao.getAllTrips()
 
-    fun paged(query: String, sort: String, limit: Int, offset: Int): LiveData<List<Trip>> =
-        dao.searchPaged(query, sort, limit, offset)
-
-    fun all(): LiveData<List<Trip>> = dao.getAll()
-
-    fun totals(): Pair<LiveData<Double?>, LiveData<Double?>> = dao.totalIncome() to dao.totalCost()
-
-    suspend fun insert(trip: Trip) = dao.insert(trip)
-    suspend fun update(trip: Trip) = dao.update(trip)
-    suspend fun delete(trip: Trip) = dao.delete(trip)
+    suspend fun insertTrip(trip: Trip) = tripDao.insertTrip(trip)
+    suspend fun deleteTrip(trip: Trip) = tripDao.deleteTrip(trip)
 }
