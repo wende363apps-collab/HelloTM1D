7c80bc618f791aa8905fb2bb612fa9ab68aa8e70
wende363apps-collab <wende363apps@gmail.com>
2025-09-22 21:44:12 +0300

    Update TripRepository.kt

--- app/src/main/java/com/truckmanager/app/TripRepository.kt
+++ app/src/main/java/com/truckmanager/app/TripRepository.kt
@@ -6,5 +6,6 @@ class TripRepository(private val tripDao: TripDao) {
     val allTrips: LiveData<List<Trip>> = tripDao.getAllTrips()
 
     suspend fun insertTrip(trip: Trip) = tripDao.insertTrip(trip)
+
     suspend fun deleteTrip(trip: Trip) = tripDao.deleteTrip(trip)
 }
